a7f6a2a228145758b6b2403ea3f299ccc2bd122e
Yaroslav <missgaming322@gmail.com>
2022-04-11 22:27:22 +0300

    dobavil v js nemnogo logoki

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 "use strict";
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '');
 
 
-console.log(2 + 2 * 2 !== '6');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из последних просмотренных фильмов', ''),
+      d = prompt('На сколько оцените его', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+
+    console.log(personalMovieDB);
+

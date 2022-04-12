30ea78c47905b320f2a71f8e361f50db01a6b5aa
Yaroslav <missgaming322@gmail.com>
2022-04-12 07:43:07 +0300

    izychil cikli

--- js/script.js
+++ js/script.js
@@ -1,38 +1,22 @@
 "use strict";
 
 
-// if (1) {
-//     console.log('Ok');
-// } else {
-//     console.log('Error');
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
 // }
-
-
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Error too');
-// } else {
-//     console.log('Ok');
+// do {
+//     console.log(num);
+//     num++;
 // }
+// while (num < 55);
 
-// (num ==-50) ? console.log('Ok') : console.log('Error');
-
-
-const num = 51;
-
-switch (num) {
-    case 49:
-        console.log("Неверно");
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('В точку');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
 }

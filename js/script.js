261e2616427041982812b6d15df47b2013f08407
Yaroslav <missgaming322@gmail.com>
2022-04-12 07:27:37 +0300

    Proshel ysloviya v js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 "use strict";
-const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '');
+
+
+// if (1) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
 
 
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Error too');
+// } else {
+//     console.log('Ok');
+// }
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его', '');
-
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+// (num ==-50) ? console.log('Ok') : console.log('Error');
 
 
-    console.log(personalMovieDB);
+const num = 51;
 
+switch (num) {
+    case 49:
+        console.log("Неверно");
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}

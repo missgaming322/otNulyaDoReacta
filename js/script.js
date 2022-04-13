5e651cb0fb32140c66ffc083abdbd114b8b83dfb
Yaroslav <missgaming322@gmail.com>
2022-04-13 19:57:09 +0300

    19 lesson | collback done

--- js/script.js
+++ js/script.js
@@ -1,70 +1,15 @@
 "use strict";
 
-let numberOfFilms;
-
-function start () {
-    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', ''); 
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', ''); 
-    }
+function first() {
+    //Do something
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
 }
 
-start();
-
-console.log(2 + 2 * 2 !== '6'); 
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов', ''),
-              b = prompt('На сколько оцените его', '');
-        if (a != null && b != null && a !='' && b !='' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('Error');
-            i--;
-        }
-    }
-
-}
-rememberMyFilms();
-
- function detectPersonalLenel () {
-    if (personalMovieDB.count < 10) {
-        alert('Вы мало смотрите');
-    } else if (30 >= personalMovieDB.count >= 10) {
-        alert('Вы дефолтный зритель');
-    } else {
-        alert('Да вы истиный киноман');
-    }
-}
-detectPersonalLenel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    } else {
-        alert('Ну я хуй его знает');
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres () {
-    for (let i = 1; i <= 3; i++) {
-
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
-    }
+function second () {
+    console.log(2);
 }
 
-writeYourGenres();
-
-    console.log(personalMovieDB);
+first();
+second();

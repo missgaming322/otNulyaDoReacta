0cca8b973c20bd338187f742d80abc4c8a83bece
Yaroslav <missgaming322@gmail.com>
2022-04-13 19:58:41 +0300

    19 lesson | update

--- js/script.js
+++ js/script.js
@@ -13,3 +13,14 @@ function second () {
 
 first();
 second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу : ${lang}`);
+    callback();
+}
+
+function done () {
+    console.log("Я прошел этот урок!");
+}
+
+learnJS('JavaScript', done);

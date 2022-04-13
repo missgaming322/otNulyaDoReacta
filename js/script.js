8611afbd96cac1fa9e5ccdc94b6745d1d1b1f070
Yaroslav <missgaming322@gmail.com>
2022-04-13 21:30:47 +0300

    21 lesson | izychil metodi massiva

--- js/script.js
+++ js/script.js
@@ -1,44 +1,40 @@
 "use strict";
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red',
-    },
-    makeTest: function() {
-        console.log('Test');
-    }
-};
+const arr = [1, 23, 13, 6, 8,];
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+console.log(arr);
 
-options.makeTest();
+arr.sort(compareNum);
+console.log(arr);
 
-const {border, bg} = options.colors;
+function compareNum(a, b) {
+    return a - b;
+}
+// console.log(arr);
 
-console.log(border);
+// arr.forEach(function(item, i, arr){
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
 
-// console.log(options["colors"]["border"]);
-
-// delete options.name;
-
-// console.log(options);
-
-let counter = 0;
+// // // arr.pop();
+// // arr.push(10);
 
 
-// for (let key in options ) {
-//     if (typeof(options[key]) ==='object' ) {
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//             counter++;
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//         counter++;
-//     }
+// // console.log(arr);
+
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+// // }
+
+// for (let value of arr) {
+//     console.log(value);
 // }
-console.log(Object.keys(options).length);
 
+// const str = prompt('', '');
+// const products = str.split(', ');
+// products.sort();
 
+// console.log(products.join('; '));

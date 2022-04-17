704f3d866447c9075f6cbdb7a67f4dbdc929561c
Yaroslav <missgaming322@gmail.com>
2022-04-17 10:23:04 +0300

    23

--- js/script.js
+++ js/script.js
@@ -11,4 +11,25 @@ let strObj = new String(str);
 console.dir([1,2,3]);
 
 
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
+    }
+};
+
+const john = Object.create(soldier);
+
+// const john = {
+//     health: 100
+// };
+
+john.__proto__ = soldier;
+
+Object.setPrototypeOf(john, soldier);
+
+// console.log(john.armor);
+john.sayHello();
+
 
